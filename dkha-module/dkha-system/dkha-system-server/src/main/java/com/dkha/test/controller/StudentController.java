99f339a6287e2174511012cec9f1b0deaf1654cd
vegool <274461951@qq.com>
2021-04-15 09:37:13 +0800

    20210414

--- dkha-module/dkha-system/dkha-system-server/src/main/java/com/dkha/test/controller/StudentController.java
+++ dkha-module/dkha-system/dkha-system-server/src/main/java/com/dkha/test/controller/StudentController.java
@@ -21,10 +21,10 @@ public class StudentController {
         return student;
     }
 
-    @GetMapping("{name}")
-    public Object select(@PathVariable String name){
-//        List<Student> list = studentService.list();
-        List<Student> list = studentService.selectByName(name);
+    @GetMapping
+    public Object select(){
+        List<Student> list = studentService.list();
+//        List<Student> list = studentService.selectByName(name);
         return list;
     }
 
